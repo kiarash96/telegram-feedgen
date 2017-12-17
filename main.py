f6fb82f4cbd84db5c5fe78daba08658bcab04da1
Kiarash Korki <kiarash.96@gmail.com>
2017-12-17 21:38:09 +0330

    Implement joining to a channel

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ import re
 import config
 
 from telethon import TelegramClient
+from telethon.tl.functions.channels import JoinChannelRequest
 
 
-def add_channel(username):
-   pass
+def add_channel(client, username):
+    channel = client.get_entity(username)
+    try:
+        client.invoke(JoinChannelRequest(channel))
+    except ValueError:
+        print("Wrong username {}!".format(username))
 
 
 def handle_update(update):
@@ -31,7 +36,7 @@ def main():
             result = re.match(r"^join\s+@?(\w+)", command)
             if result:
                 username = result.group(1)
-                add_channel(username)
+                add_channel(client, username)
 
     client.disconnect()
 
