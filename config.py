1e804641f54addedaac07ccb5276bdc6858445db
Kiarash Korki <kiarash.96@gmail.com>
2017-12-17 21:30:36 +0330

    Add code to connect to telegram server

---
+++ config.py
@@ -0,0 +1,6 @@
+
+session_name = "TelegramRss"
+api_id = 111111
+api_hash = ''
+phone = ''
+output_dir = "./output/"
--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 import re
 
+import config
+
+from telethon import TelegramClient
+
 
 def add_channel(username):
+   pass
+
+
+def handle_update(update):
     pass
 
 
 def main():
+    client = TelegramClient(config.session_name, config.api_id, config.api_hash, update_workers=1)
+    client.connect()
+
+    if not client.is_user_authorized():
+        client.send_code_request(config.phone)
+        client.sign_in(phone=config.phone, code=input("Enter code: "))
+
+    client.add_update_handler(handle_update)
+
     while True:
         command = input(">> ")
         if command == "exit":
@@ -15,6 +32,8 @@ def main():
             if result:
                 username = result.group(1)
                 add_channel(username)
+
+    client.disconnect()
 
 
 if __name__ == "__main__":
